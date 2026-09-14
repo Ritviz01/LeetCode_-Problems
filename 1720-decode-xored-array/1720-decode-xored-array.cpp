71993f9da8180a4a968e17eb550736b73828b5bb
Ritviz Tiwari <ritviztiwari07@gmail.com>
2026-09-14 14:26:42 +0530

    Time: 2 ms (22.59%), Space: 30.6 MB (23.24%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+         vector<int> ans;
+
+         ans.push_back(first);
+
+         for( int i =0 ;i<encoded.size();i++){
+            int next = encoded[i]^ans[i];
+
+            ans.push_back(next);
+         }
+        return ans;
+    }
+};
